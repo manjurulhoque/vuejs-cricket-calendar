09ac827e6067874485c5a56a001a485ad4aefab9
manjurulhoque <rumimanzurulhoque@gmail.com>
2017-02-05 19:14:47 +0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container" id="app">
+    <News></News>
+  </div>
+</template>
+
+<script>
+import News from './components/News'
+
+export default {
+  name: 'app',
+  components: {
+    News
+  }
+}
+</script>
+
+<style>
+  #app {
+    padding-top: 20px
+  }
+</style>
---
+++ src/components/News.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="news">
+  <div>
+    <div class="jumbotron">
+      <h2><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Cricket Calendar</h2>
+      <table class="table table-striped">
+        <thead>
+          <tr class="info">
+            <th>Name</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="match in matches">
+            <td>{{match.name}}</td>
+            <td>{{match.date}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'news',
+  data() {
+    return {
+      matches: []
+    }
+  },
+  created: function() {
+    this.$http.get('http://cricapi.com/api/matchCalendar?apikey=hfrDfOnDgIcSUAKzXxMbgTXi89H2')
+      .then(response => {
+        this.matches = response.data.data;
+        console.log(this.matches);
+      });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
